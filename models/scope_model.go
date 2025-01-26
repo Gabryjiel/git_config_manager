5b2397deba783d87eb02c7074b8f9b4705320dd1
Gabryjiel <gabriel.kudyba@gmail.com>
2025-01-26 20:07:31 +0100

    feat: reword a ScopeModel
    It works quite well now

--- models/scope_model.go
+++ models/scope_model.go
@@ -2,77 +2,76 @@ package models
 
 import (
 	"github.com/Gabryjiel/git_config_manager/git"
+	"github.com/Gabryjiel/git_config_manager/utils"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 type ScopeModel struct {
-	tea.Model
 	prop      git.GitConfigProp
-	textField TextField
+	input     TextInputModel
 	cursor    MenuCursor
 	isEditing bool
 }
 
-func NewScopeModel() ScopeModel {
-	return ScopeModel{}
+func NewScopeModel() *ScopeModel {
+	return &ScopeModel{
+		input:     NewTextField(),
+		cursor:    0,
+		isEditing: false,
+	}
 }
 
-func (this ScopeModel) Init() tea.Cmd {
+func (this *ScopeModel) Init() tea.Cmd {
 	return nil
 }
 
-func (this ScopeModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (this *ScopeModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case GitConfigSetResult:
+		this.isEditing = false
+		this.prop.Values[this.getScopeFromCursor()] = this.input.GetValue()
+		this.input.Clear()
+		return this, nil
 	case git.GitConfigProp:
 		this.prop = msg
 	case tea.KeyMsg:
 		switch msg.Type {
-		case tea.KeyCtrlC:
-			return this, tea.Quit
 		case tea.KeyEsc:
-			if this.isEditing {
-				this.isEditing = false
-			} else {
-				return NewSearchModel(), nil
-			}
-		case tea.KeyRunes:
-			if this.isEditing {
-				this.textField.Value += msg.String()
-			}
-		case tea.KeyBackspace:
-			this.textField.removeLastCharacter()
-		case tea.KeyCtrlW:
-			this.textField.removeLastWord()
+			this.isEditing = false
+			return this, SwitchSubmodel(MODEL_ID_SEARCH)
 		case tea.KeyDown:
-			fallthrough
-		case tea.KeyCtrlN:
 			this.cursor.moveDown(2)
 		case tea.KeyUp:
-			fallthrough
-		case tea.KeyCtrlP:
 			this.cursor.moveUp()
 		case tea.KeyEnter:
-			this.isEditing = !this.isEditing
-
-			if !this.isEditing {
+			if this.isEditing {
+				this.isEditing = false
+				return this, CmdGitConfigSet(this.prop.GetName(), this.input.GetValue())
+			} else {
+				this.isEditing = true
+				value, ok := this.prop.Values[this.getScopeFromCursor()]
+				if ok {
+					this.input.SetValue(value)
+				}
+				return this, nil
 			}
 		}
+	}
+
+	if this.isEditing {
+		_, cmd := this.input.Update(msg)
+		return this, cmd
 	}
 	return this, nil
 }
 
-func (this ScopeModel) View() string {
+func (this *ScopeModel) View() string {
 	output := ""
 
 	output += renderHeader()
 	output += arrowCinStyle.Render(" Value: ")
-	output += this.textField.Value
-
-	if this.isEditing {
-		output += lipgloss.NewStyle().Foreground(lipgloss.ANSIColor(15)).Render("█")
-	}
-
+	output += this.input.View()
 	output += "\n\n"
 
 	localValue, _ := this.prop.Values["local"]
@@ -84,6 +83,21 @@ func (this ScopeModel) View() string {
 	output += renderPropValue(git.SCOPE_SYSTEM, systemValue, this.cursor)
 
 	return output
+}
+
+// Helpers
+
+func (this *ScopeModel) getScopeFromCursor() string {
+	switch this.cursor {
+	case 0:
+		return "local"
+	case 1:
+		return "global"
+	case 2:
+		return "system"
+	default:
+		return "local"
+	}
 }
 
 func renderPropValue(scope git.ValueScope, value string, cursor MenuCursor) string {
@@ -119,4 +133,35 @@ func renderPropValue(scope git.ValueScope, value string, cursor MenuCursor) stri
 	output += defaultStyle.Padding(0, 1).Render(left+right) + "\n"
 
 	return output
+}
+
+// Commands
+
+func CmdScopeSelectProp(prop git.GitConfigProp) tea.Cmd {
+	return func() tea.Msg {
+		return prop
+	}
+}
+
+type GitConfigSetResult struct {
+	result  bool
+	message string
+}
+
+func CmdGitConfigSet(name string, value string) tea.Cmd {
+	return func() tea.Msg {
+		content, err := utils.ExecuteCommand("git", "config", "set", name, value)
+
+		if err != nil {
+			return GitConfigSetResult{
+				message: err.Error(),
+				result:  false,
+			}
+		}
+
+		return GitConfigSetResult{
+			result:  true,
+			message: content,
+		}
+	}
 }
