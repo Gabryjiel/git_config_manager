97039d8c2b7ef13b150ece37e5120c8dd3a53979
Gabryjiel <gabriel.kudyba@gmail.com>
2025-02-04 19:34:14 +0100

    feat: expand ListModel with generics
    It's not satysfying yet

--- models/list.go
+++ models/list.go
@@ -2,16 +2,35 @@ package models
 
 import tea "github.com/charmbracelet/bubbletea"
 
-type ListModel struct{}
+type ListModel[TElement any] struct {
+	cursor     MenuCursor
+	upperBound uint
+	items      []TElement
+	renderRow  func(TElement) string
+}
 
-func (this *ListModel) Init() tea.Cmd {
+func CreateNewListModel[TElement any](renderRow func(TElement) string) ListModel[TElement] {
+	return ListModel[TElement]{
+		renderRow: renderRow,
+	}
+}
+
+func (this *ListModel[T]) Init() tea.Cmd {
 	return nil
 }
 
-func (this *ListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (this *ListModel[T]) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return this, nil
 }
 
-func (this *ListModel) View() string {
-	return ""
+func (this *ListModel[T]) View() string {
+	output := ""
+
+	for _, item := range this.items {
+		output += this.renderRow(item) + "\n"
+	}
+
+	return output
 }
+
+//
